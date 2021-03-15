50be61b4a5ed6460ea754dd049e3b106e589ce8c
Tatsuya-Inoue926 <t.i.ykts.926@ezweb.ne.jp>
2021-03-15 06:47:59 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '462f5c578905945de1feba7081f49f395a31dd852098d67780b1b4c46fba5ea2dbe477dffa78fab644249546913ff29ea92cbd042cede6ca24b978bdd7c1b9e9'
